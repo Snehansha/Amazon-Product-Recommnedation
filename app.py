4f16f488a37bd05473bb14ec7bf71741e2ae36e5
Snehansha <snehansha286@gmail.com>
2025-07-16 20:23:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import streamlit as st
 
-# Load Data
 df = pd.read_csv('amazon.csv')
 df['about_product'] = df['about_product'].fillna('')
 
-# TF-IDF and Cosine Similarity
 tfidf = TfidfVectorizer(stop_words='english')
 tfidf_matrix = tfidf.fit_transform(df['about_product'])
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
-# Recommend Function
 def recommend(index, num=5):
     sim_scores = list(enumerate(cosine_sim[index]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
@@ -20,7 +17,6 @@ def recommend(index, num=5):
     indices = [i[0] for i in sim_scores]
     return df.iloc[indices][['product_name', 'actual_price', 'rating']]
 
-# Streamlit App
 st.title("Amazon Product Recommender")
 
 product_list = df['product_name'].tolist()
